8b38438f178c7220424626141d51158cffa92d4f
Adam Fowler <adamfowler71@gmail.com>
2021-09-28 15:41:11 +0100

    Make HBMustacheParent public

--- Sources/HummingbirdMustache/Parent.swift
+++ Sources/HummingbirdMustache/Parent.swift
@@ -14,12 +14,12 @@
 
 /// Protocol for object that has a custom method for accessing their children, instead
 /// of using Mirror
-protocol HBMustacheParent {
+public protocol HBMustacheParent {
     func child(named: String) -> Any?
 }
 
 /// Extend dictionary where the key is a string so that it uses the key values to access
 /// it values
 extension Dictionary: HBMustacheParent where Key == String {
-    func child(named: String) -> Any? { return self[named] }
+    public func child(named: String) -> Any? { return self[named] }
 }
