13e0ee8357356cbdfcb9b9ab9d1463e8e5e3e013
Adam Fowler <adamfowler71@gmail.com>
2024-06-27 10:58:14 +0100

    MustacheTemplate.init?(filename:) internal

--- Sources/Mustache/Template+FileSystem.swift
+++ Sources/Mustache/Template+FileSystem.swift
@@ -15,12 +15,12 @@
 import Foundation
 
 extension MustacheTemplate {
-    /// Initialize template
+    /// Internal function to load a template from a file
     /// - Parameters
     ///   - string: Template text
     ///   - filename: File template was loaded from
     /// - Throws: MustacheTemplate.Error
-    public init?(filename: String) throws {
+    init?(filename: String) throws {
         let fs = FileManager()
         guard let data = fs.contents(atPath: filename) else { return nil }
         let string = String(decoding: data, as: Unicode.UTF8.self)
