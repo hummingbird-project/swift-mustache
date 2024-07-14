47543aaf5a1d2e784a1e6efa4e6e33ade84967d5
MahdiBM <github@mahdibm.com>
2024-07-14 12:09:05 +0330

    even better use of shared logic

--- Tests/MustacheTests/TemplateParserTests.swift
+++ Tests/MustacheTests/TemplateParserTests.swift
@@ -58,53 +58,41 @@ final class TemplateParserTests: XCTestCase {
     }
 }
 
-private func isEqual(lhs: MustacheTemplate, rhs: MustacheTemplate) -> Bool {
-    lhs.tokens == rhs.tokens
+extension MustacheTemplate {
+    public static func == (lhs: MustacheTemplate, rhs: MustacheTemplate) -> Bool {
+        lhs.tokens == rhs.tokens
+    }
 }
 
 #if hasFeature(RetroactiveAttribute)
-extension MustacheTemplate: @retroactive Equatable {
-    public static func == (lhs: MustacheTemplate, rhs: MustacheTemplate) -> Bool {
-        isEqual(lhs: lhs, rhs: rhs)
-    }
-}
+extension MustacheTemplate: @retroactive Equatable {}
 #else
-extension MustacheTemplate: Equatable {
-    public static func == (lhs: MustacheTemplate, rhs: MustacheTemplate) -> Bool {
-        isEqual(lhs: lhs, rhs: rhs)
-    }
-}
+extension MustacheTemplate: Equatable {}
 #endif
 
-private func isEqual(lhs: MustacheTemplate.Token, rhs: MustacheTemplate.Token) -> Bool {
-    switch (lhs, rhs) {
-    case (.text(let lhs), .text(let rhs)):
-        return lhs == rhs
-    case (.variable(let lhs, let lhs2), .variable(let rhs, let rhs2)):
-        return lhs == rhs && lhs2 == rhs2
-    case (.section(let lhs1, let lhs2, let lhs3), .section(let rhs1, let rhs2, let rhs3)):
-        return lhs1 == rhs1 && lhs2 == rhs2 && lhs3 == rhs3
-    case (.invertedSection(let lhs1, let lhs2, let lhs3), .invertedSection(let rhs1, let rhs2, let rhs3)):
-        return lhs1 == rhs1 && lhs2 == rhs2 && lhs3 == rhs3
-    case (.partial(let name1, let indent1, _), .partial(let name2, let indent2, _)):
-        return name1 == name2 && indent1 == indent2
-    case (.contentType(let contentType), .contentType(let contentType2)):
-        return type(of: contentType) == type(of: contentType2)
-    default:
-        return false
+extension MustacheTemplate.Token {
+    public static func == (lhs: MustacheTemplate.Token, rhs: MustacheTemplate.Token) -> Bool {
+        switch (lhs, rhs) {
+        case (.text(let lhs), .text(let rhs)):
+            return lhs == rhs
+        case (.variable(let lhs, let lhs2), .variable(let rhs, let rhs2)):
+            return lhs == rhs && lhs2 == rhs2
+        case (.section(let lhs1, let lhs2, let lhs3), .section(let rhs1, let rhs2, let rhs3)):
+            return lhs1 == rhs1 && lhs2 == rhs2 && lhs3 == rhs3
+        case (.invertedSection(let lhs1, let lhs2, let lhs3), .invertedSection(let rhs1, let rhs2, let rhs3)):
+            return lhs1 == rhs1 && lhs2 == rhs2 && lhs3 == rhs3
+        case (.partial(let name1, let indent1, _), .partial(let name2, let indent2, _)):
+            return name1 == name2 && indent1 == indent2
+        case (.contentType(let contentType), .contentType(let contentType2)):
+            return type(of: contentType) == type(of: contentType2)
+        default:
+            return false
+        }
     }
 }
 
 #if hasFeature(RetroactiveAttribute)
-extension MustacheTemplate.Token: @retroactive Equatable {
-    public static func == (lhs: MustacheTemplate.Token, rhs: MustacheTemplate.Token) -> Bool {
-        isEqual(lhs: lhs, rhs: rhs)
-    }
-}
+extension MustacheTemplate.Token: @retroactive Equatable {}
 #else
-extension MustacheTemplate.Token: Equatable {
-    public static func == (lhs: MustacheTemplate.Token, rhs: MustacheTemplate.Token) -> Bool {
-        isEqual(lhs: lhs, rhs: rhs)
-    }
-}
+extension MustacheTemplate.Token: Equatable {}
 #endif
