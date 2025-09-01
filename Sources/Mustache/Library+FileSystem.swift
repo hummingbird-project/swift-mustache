534f9f9e60ccc1ecc947cababb71c5f91497f4a2
Adam Fowler <adamfowler71@gmail.com>
2025-09-01 09:59:05 +0100

    Add testPartialInSubfolder (#72)
    * Add testPartialInSubfolder
    
    * Replace windows folder separator with mac/linux folder separator

--- Sources/Mustache/Library+FileSystem.swift
+++ Sources/Mustache/Library+FileSystem.swift
@@ -31,7 +31,11 @@ extension MustacheLibrary {
                 guard let template = try MustacheTemplate(filename: directory + path) else { continue }
                 // drop ".mustache" from path to get name
                 let name = String(path.dropLast(extWithDot.count))
+                #if os(Windows)
+                templates[name.replacingOccurrences(of: "\\", with: "/")] = template
+                #else
                 templates[name] = template
+                #endif
             } catch let error as MustacheTemplate.ParserError {
                 throw ParserError(filename: path, context: error.context, error: error.error)
             }
