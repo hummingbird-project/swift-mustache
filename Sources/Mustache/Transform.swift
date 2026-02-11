8e3d089777eac9ccc9ee86b48982998d767f0b50
Adam Fowler <adamfowler71@gmail.com>
2026-02-11 19:14:13 +0000

    Function header, LICENSE file rename (#77)

--- Sources/Mustache/Transform.swift
+++ Sources/Mustache/Transform.swift
@@ -1,16 +1,10 @@
-//===----------------------------------------------------------------------===//
 //
 // This source file is part of the Hummingbird server framework project
-//
-// Copyright (c) 2021-2021 the Hummingbird authors
-// Licensed under Apache License v2.0
+// Copyright (c) the Hummingbird authors
 //
 // See LICENSE.txt for license information
-// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
-//
 // SPDX-License-Identifier: Apache-2.0
 //
-//===----------------------------------------------------------------------===//
 
 /// Objects that can have a transforms run on them. Mustache transforms are specific to this implementation
 /// of Mustache. They allow you to process objects before they are rendered.
